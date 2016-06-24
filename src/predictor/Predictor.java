ccbe51c90e7771f6af9ad907a20d763b4de19ddf
kevinchenghua <s9822108@m98.nthu.edu.tw>
2016-06-24 11:10:04 +0800

    reassign the folder

--- src/predictor/Predictor.java
+++ src/predictor/Predictor.java
@@ -5,11 +5,11 @@ import weka.core.*;
 class Predictor {
   public static void main(String [] args){
     
-    List<UserMovieInstance> userMovie = Preprocessor.readDataFromCSV("..\\dataset\\test.csv");
+    List<UserMovieInstance> userMovie = Preprocessor.readDataFromCSV("res\\dataset\\test.csv");
     List<UserMoviesInstance> userMovies = Preprocessor.joinUser(userMovie);
     TrainTestPair dataPair = Preprocessor.splitData(userMovies,0.001,1);
     
-    for(UserMoviesInstance instance : abc.getTrainingData()) {
+    for(UserMoviesInstance instance : dataPair.getTrainingData()) {
       System.out.println(instance);
     }
   }
